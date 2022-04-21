0b8d1c293f8d236d08dfe9ddeb8e05f6bd2d3627
kzkman <jackigra@gmail.com>
2022-04-21 13:59:51 +0300

    1

---
+++ test/test_api.py
@@ -0,0 +1,23 @@
+import pytest
+import os
+import sys
+from app import app
+
+path = os.path.abspath('.')
+sys.path.insert(1, path)
+
+
+def test_api_posts():
+    response = app.test_client().get('/api/posts/')
+    posts = response.json
+    keys_to_check = ('poster_name', 'poster_avatar', 'pic', 'content', 'views_count', 'likes_count', 'pk')
+    assert isinstance(posts, list), "Ошибка API (загрузка постов): выгружается не список"
+    assert sorted(tuple(posts[0].keys())) == sorted(keys_to_check), "Ошибка API (загрузка постов): нет нужных ключей"
+
+
+def test_api_post_by_id():
+    response = app.test_client().get('/api/posts/1')
+    post = response.json
+    keys_to_check = ('poster_name', 'poster_avatar', 'pic', 'content', 'views_count', 'likes_count', 'pk')
+    assert isinstance(post, dict), "Ошибка API (загрузка поста по ID): выгружается не словарь"
+    assert sorted(tuple(post.keys())) == sorted(keys_to_check), "Ошибка API (загрузка поста по ID): нет нужных ключей"
