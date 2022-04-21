27d7a12dc1c4e9e09baa6da580baaf564c1823e2
Kzkus <96435922+Kzkus@users.noreply.github.com>
2022-04-21 13:56:56 +0300

    Update api.py

--- api/api.py
+++ api/api.py
@@ -5,12 +5,12 @@ api_blueprint = Blueprint('api_blueprint', __name__)
 
 
 @api_blueprint.route("/posts/")
-def get_posts_all():
+def api_get_posts_all():
     posts = get_posts_all()
     return jsonify(posts), {'Content-type': 'application/json; charset=utf-8'}
 
 
-@api_blueprint.route("/posts/<int:post_id>")
-def get_post_by_pk(pk):
+@api_blueprint.route("/posts/<int:pk>")
+def api_get_post_by_pk(pk):
     post = get_post_by_pk(pk)
     return jsonify(post), {'Content-type': 'application/json; charset=utf-8'}
